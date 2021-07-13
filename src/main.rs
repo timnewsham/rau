d197ba5efa1080a57de94b3a36ddf6b25187da99
Tim Newsham <tim.newsham@gmail.com>
2021-07-12 18:30:45 -1000

    gen file for testing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 
 mod ascii;
+mod file;
 mod freq;
 mod gen;
 
-fn main() {
+fn record(gen: &mut impl gen::Gen, seconds: f64, tape: &mut Vec<f64>) {
+    let samples = (seconds * freq::SAMPLE_RATE) as usize;
+    for _ in 1..samples {
+        tape.push(gen.gen());
+        gen.advance();
+    }
+}
+
+fn visual_check() {
     ascii::plot(&mut gen::HarmonicGenerator::new_sine(2.0));
     ascii::plot(&mut gen::HarmonicGenerator::new_triangle(2.0, 10));
     ascii::plot(&mut gen::HarmonicGenerator::new_saw_up(2.0, 10));
@@ -18,4 +27,22 @@ fn main() {
     ascii::plot(&mut gen);
     gen.set_sine();
     ascii::plot(&mut gen);
+}
+
+// sox -r 44100 -e signed -B -b 16 -c 1 out.s16 out.wav
+fn make_file() {
+    let mut tape = Vec::new();
+    let mut gen = gen::HarmonicGenerator::new_saw_up(10000.0, 40);
+    //let mut gen = gen::HarmonicGenerator::new_sine(1.0);
+
+    gen.set_freq(440.0);
+    record(&mut gen, 0.25, &mut tape);
+    gen.set_freq(880.0);
+    record(&mut gen, 0.25, &mut tape);
+    file::writeFile("out.s16", &tape);
+}
+
+fn main() {
+    make_file();
+    visual_check();
 }
