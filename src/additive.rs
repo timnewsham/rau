acdf7e952b935180ef7fa865619a51cf52a582bb
Tim Newsham <tnewsham@bishopfox.com>
2021-08-03 13:35:21 -1000

    cleaner series gen func

--- src/additive.rs
+++ src/additive.rs
@@ -8,63 +8,35 @@ use crate::gen;
 pub enum Function{ SIN, TRI, SAWUP, SAWDOWN, SQUARE }
 
 // Param in a harmonic series
-// note: k is usually but need not be an integer.
 pub struct HarmonicParam {
-    pub k: f64,
+    pub k: usize,
     pub amp: f64,
 }
 
-// Easier name for (-1)^k
-fn neg1_k(k: f64) -> f64 {
-    f64::powf(-1.0, k)
+// shorthand for (-1)^k
+fn powneg1(k: usize) -> f64 {
+    (-1.0_f64).powf(k as f64)
 }
 
-fn sine_series() -> Vec<HarmonicParam> {
-    vec![HarmonicParam{ k: 1.0, amp: 1.0 }]
-}
-
-fn saw_up_series(n: u64) -> Vec<HarmonicParam> {
-    debug_assert!(0 < n && n < 100);
-    let mut weights = Vec::new();
-    for k in 1..=n {
-        let kf = k as f64;
-        let w = -2.0 * neg1_k(kf) /(kf * PI);
-        weights.push(HarmonicParam{ k: k as f64, amp: w });
+fn get_series(func: Function, n: usize) -> Vec<HarmonicParam> {
+    match func {
+        Function::SIN => vec![HarmonicParam{ k: 1, amp: 1.0 }],
+        Function::SAWUP => (1..=n).map(|k|
+                HarmonicParam{ k: k, amp: -2.0 * powneg1(k) / (k as f64 * PI) }
+            ).collect(),
+        Function::SAWDOWN => (1..=n).map(|k|
+                HarmonicParam{ k: k, amp: 2.0 * powneg1(k) / (k as f64 * PI) }
+            ).collect(),
+        Function::TRI => (1..=n).map(|nn| {
+                let k = 2*nn - 1; // odd harmonics
+                HarmonicParam{ k: k, amp: 8.0 * powneg1((k-1)/2) / (k as f64 * PI).powf(2.0) }
+            }).collect(),
+        Function::SQUARE => (1..=n).map(|nn| {
+                let k = 2*nn - 1; // odd harmonics
+                HarmonicParam{ k: k, amp: -4.0 * powneg1(k) / (k as f64 * PI) }
+            }).collect(),
     }
-    weights
 }
-
-fn saw_down_series(n: u64) -> Vec<HarmonicParam> {
-    let mut params = saw_up_series(n);
-    for p in params.iter_mut() {
-        p.amp *= -1.0;
-    }
-    params
-}
-
-fn triangle_series(n: u64) -> Vec<HarmonicParam> {
-    let mut weights = Vec::new();
-    for kk in 1..=n {
-        let k = 2*kk - 1; // odd harmonics only
-        let kf = k as f64;
-        let w = 8.0 * neg1_k((kf-1.0)/2.0) / ((kf * PI).powf(2.0));
-        weights.push(HarmonicParam{ k: k as f64, amp: w });
-    }
-    weights
-}
-
-fn square_series(n: u64) -> Vec<HarmonicParam> {
-    debug_assert!(0 < n && n < 100);
-    let mut weights = Vec::new();
-    for kk in 1..=n {
-        let k = 2*kk - 1; // odd harmonics only
-        let kf = k as f64;
-        let w = -4.0 * neg1_k(kf) / (kf * PI);
-        weights.push(HarmonicParam{ k: k as f64, amp: w });
-    }
-    weights
-}
-
 
 // An additive generator generates a signal as a sum of SIN waves.
 pub struct Gen {
@@ -77,19 +49,9 @@ pub struct Gen {
     velocity: RadPS,
 }
 
-fn get_series(typ: Function, n: u64) -> Vec<HarmonicParam> {
-    match typ {
-        Function::SIN => sine_series(),
-        Function::TRI => triangle_series(n),
-        Function::SAWUP => saw_up_series(n),
-        Function::SAWDOWN => saw_down_series(n),
-        Function::SQUARE => square_series(n),
-    }
-}
-
 #[allow(dead_code)]
 impl Gen {
-    pub fn new(typ: Function, freq: impl Into<RadPS>, n: u64) -> Self {
+    pub fn new(typ: Function, freq: impl Into<RadPS>, n: usize) -> Self {
         Self {
             phase: 0.0,
             velocity: freq.into(),
@@ -97,7 +59,7 @@ impl Gen {
         }
     }
 
-    pub fn set_func(&mut self, typ: Function, n: u64) {
+    pub fn set_func(&mut self, typ: Function, n: usize) {
         self.series = get_series(typ, n);
     }
 
@@ -109,7 +71,7 @@ impl Gen {
     pub fn cost(&self) -> usize {
         let mut cost = 0;
         for param in self.series.iter() {
-            if param.k * self.velocity.0 <= MAXRADPS {
+            if param.k as f64 * self.velocity.0 <= MAXRADPS {
                 cost += 1;
             }
         }
@@ -131,8 +93,8 @@ impl gen::Gen for Gen {
         for param in self.series.iter() {
             // disallow aliasing
             // XXX would be better to trim series once instead of each gen
-            if param.k * self.velocity.0 <= MAXRADPS {
-                x += param.amp * (param.k * self.phase).sin();
+            if param.k as f64 * self.velocity.0 <= MAXRADPS {
+                x += param.amp * (param.k as f64 * self.phase).sin();
             }
         }
         x
