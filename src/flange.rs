8681b506ccadf08d0f88af147f59c342c23b2650
Tim Newsham <tnewsham@bishopfox.com>
2021-08-07 13:14:34 -1000

    tuned flanger a little based off flangeviz experiments

--- src/flange.rs
+++ src/flange.rs
@@ -13,32 +13,31 @@ pub struct Flange {
     val: f64,
 }
 
-const MAXDELAY: f64 = 10e-3; // in seconds
+const MAXDELAY: f64 = 1e-3; // in seconds, about 25 comb notches
 
 impl Flange {
     pub fn from_cmd(args: &Vec<&str>) -> Result<ModRef, String> {
-        if args.len() != 7 {
-            return Err(format!("usage: {} functype freq manual width dry feedback", args[0]));
+        if args.len() != 6 {
+            return Err(format!("usage: {} functype freq manual width feedback", args[0]));
         }
         let func = parse::<Function>("functype", args[1])?;
         let freq = parse::<f64>("freq", args[2])?;
         let manual = parse::<f64>("manual", args[3])?;
         let width = parse::<f64>("width", args[4])?;
-        let dry = parse::<f64>("dry", args[5])?;
-        let fb = parse::<f64>("feedback", args[6])?;
+        let fb = parse::<f64>("feedback", args[5])?;
 
-        // XXX sanity check manual, width, dry, fb
-        Ok( modref_new(Self::new(func, Hz(freq), manual, width, dry, fb)) )
+        // XXX sanity check manual, width, fb
+        Ok( modref_new(Self::new(func, Hz(freq), manual, width, fb)) )
     }
 
     // manual is fraction of MAXDELAY
     // width is fraction, at 0.99, lfo sweeps full range from 0 to MAXDELAY (centered at manual)
-    pub fn new(func: Function, freq: impl Into<RadPS>, manual: f64, width: f64, dry: f64, fb: f64) -> Self {
+    pub fn new(func: Function, freq: impl Into<RadPS>, manual: f64, width: f64, fb: f64) -> Self {
         assert!(0.0 < manual && manual < 1.0);
         assert!(0.0 < width.abs() && width.abs() < 1.0); // XXX allow negative width for inverting phase of lfo?
 
         let lfo = Osc::new(func, freq);
-        let delay = Delay::new(Sec(MAXDELAY), dry, fb);
+        let delay = Delay::new(Sec(MAXDELAY), 1.0, fb);
         Flange { 
             delay: delay,
             lfo: lfo,
@@ -66,12 +65,8 @@ impl Flange {
         self.delay.set_fb(fb);
     }
 
-    pub fn set_dry(&mut self, dry: f64) {
-        self.delay.set_dry(dry);
-    }
-
     pub fn set_input(&mut self, v: f64) {
-        self.delay.set_input(v);
+        self.delay.set_input(0.5 * v);
     }
     pub fn advance(&mut self) -> f64 {
         // delay oscillates between m-w and m+w and most be between 0 and MAXDELAY
